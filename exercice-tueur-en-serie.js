856411ffbbf90c3c14a7d7a27d11182b48e06480
Camille Dupouy <camille.amandine.dupouy@gmail.com>
2022-11-09 16:58:26 +0100

    suite

--- exercice-tueur-en-serie.js
+++ exercice-tueur-en-serie.js
@@ -10,10 +10,9 @@ const caracteristique = ["nerd","blond","timide","curieux","sportif"]
 
 class Personnage{ 
 
-    constructor(nom,caracteristique,degats) {
+    constructor(nom,caracteristique,luck) {
         this.nom = nom
         this.caracteristique= caracteristique
-        this.degats= degats
         this.luck = luck
     }
 } 
@@ -21,55 +20,51 @@ class Personnage{
 let personnage1 = new Personnage(
    nom[Math.floor(Math.random()*nom.length)],
    caracteristique[Math.floor(Math.random()*caracteristique.length)],
-   luck[Math.random()]
+   Math.random()
 )
 
 let personnage2 = new Personnage(
     nom[Math.floor(Math.random()*nom.length)],
     caracteristique[Math.floor(Math.random()*caracteristique.length)],
-    luck[Math.random()]
+    Math.random()
 )
 
 let personnage3 = new Personnage(
     nom[Math.floor(Math.random()*nom.length)],
     caracteristique[Math.floor(Math.random()*caracteristique.length)],
-    luck[Math.random()]
+    Math.random()
 )
 
 let personnage4 = new Personnage(
     nom[Math.floor(Math.random()*nom.length)],
     caracteristique[Math.floor(Math.random()*caracteristique.length)],
-    luck[Math.random()]
+    Math.random()
 )
 
 let personnage5 = new Personnage(
     nom[Math.floor(Math.random()*nom.length)],
-    caracteristique[Math.floor(Math.random()*caracteristique.length)]
+    caracteristique[Math.floor(Math.random()*caracteristique.length)],
+    Math.random()
 )
 
-Degatstueur(Personnage){ 
-    
-    if(personnage.luck)
 
-        {tueur.pointvie= tueur.pointvie-this.dégats 
-        console.log(Personnage.nom + " a évité et inflige " + this.degats+ " de degats au tueur.")} 
-    
-    else { 
-          
-          console.log(Personnage.nom + " est mort." )}
-    }
-}
 
 while (tueur.pointvie>0 ) {
     
-    tueur
-    if(phyllali.pv>0) {
-        phyllali.attackPokemon(ronflex)
-    }  
-    else{
-        console.log(this.nom + "est mort") 
-        break
+    if(personnage.luck> Math.random()){
+        
+        tueur.pointvie= tueur.pointvie-this.dégats 
+        console.log(Personnage.nom + " a évité et inflige " + this.degats+ " de degats au tueur.")
     } 
-}
+    
+    else{
+        tueur.pointvie= tueur.pointvie-this.dégats 
+        console.log(Personnage.nom + " inflige " + this.degats+ " de degats au tueur mais meurt.")
+        }
+    
+    else { 
+        console.log(Personnage.nom + " est mort." )
+        }
 
-console.log(Personnage)
+    console.log(Personnage)
+}
